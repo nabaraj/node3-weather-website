e572e2c1eeabe0759422e324d29b27de4dae2b75
nabaraj <nabaraj2k@gmail.com>
2019-10-20 21:51:33 +0530

    added console

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-//console.log("Client side javascript file is loaded!");
+console.log("Client side javascript file is loaded!");
 // fetch("http://puzzle.mead.io/puzzle")
 //   .then(response => {
 //     response.json().then(data => {
